c4bee0880c4ca71f9815d466275c303e84756738
Naganandhini R <167079907+Naganandhini-R@users.noreply.github.com>
2025-09-12 22:37:41 +0530

    second

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ await connectDB();
 await connectCloudinary();
 
 // Middleware
-app.use(cors({ origin: process.env.CLIENT_URL || '*', credentials: true }));
+app.use(cors({ origin: process.env.CLIENT_URL , credentials: true }));
 app.use(express.json());
 app.use(clerkMiddleware());
 
