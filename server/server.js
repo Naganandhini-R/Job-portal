650734882fc7f6244f91c07d757bb9f95fc2921d
Naganandhini R <167079907+Naganandhini-R@users.noreply.github.com>
2025-09-13 21:09:26 +0530

    second

--- server/server.js
+++ server/server.js
@@ -1,4 +1,3 @@
-import './config/instrument.js';
 import express from 'express';
 import cors from 'cors';
 import connectDB from './config/db.js';
@@ -8,33 +7,35 @@ import jobRoutes from './routes/jobRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import savedJobRoutes from "./routes/savedJobRoutes.js";
 import { clerkMiddleware } from '@clerk/express';
-import * as Sentry from '@sentry/node';
 import 'dotenv/config';
+import * as Sentry from '@sentry/node';
 
 const app = express();
 
-// Connect DB + Cloudinary
+// DB + Cloudinary
 await connectDB();
 await connectCloudinary();
 
 // Middleware
-app.use(cors({ origin: process.env.CLIENT_URL , credentials: true }));
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
 app.use(clerkMiddleware());
 
-// Routes
-app.get('/api', (req, res) => res.send('✅ API working'));
-app.use("/api/api", (req, res, next) => {
-  req.url = req.url.replace(/^\/api/, "");
+// Optional: prevent caching
+app.use((req, res, next) => {
+  res.set("Cache-Control", "no-store");
   next();
 });
+
+// Routes
+app.get('/api', (req, res) => res.send('✅ API working'));
 app.use('/api/company', companyRoutes);
 app.use('/api/jobs', jobRoutes);
 app.use('/api/users', userRoutes);
-app.use("/api/saved-jobs", savedJobRoutes);
+app.use('/api/saved-jobs', savedJobRoutes);
 
 // Sentry error handler
 Sentry.setupExpressErrorHandler(app);
 
-//  Remove app.listen() for Vercel
+// Export for Vercel
 export default app;
