1b9bf913f8072ac764696cd159815f5861909e27
Naganandhini R <167079907+Naganandhini-R@users.noreply.github.com>
2025-09-13 21:10:08 +0530

    second

--- client/src/api.js
+++ client/src/api.js
@@ -1,9 +1,9 @@
-// api.js
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL || "http://localhost:5000",
+  baseURL: import.meta.env.VITE_BACKEND_URL, // https://job-portal.vercel.app
   headers: { "Content-Type": "application/json" },
+  withCredentials: true, // needed for cookies if used
 });
 
 export default api;
